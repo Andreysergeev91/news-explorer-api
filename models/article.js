593dd52ec789896dd924b68f768d3367e2c1bed1
Andrey Sergeev <andreyser07@gmail.com>
2020-06-25 19:24:28 +0700

    Add model and schema 'article'
    Add Mongoose  model and schema 'article'

---
+++ models/article.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const validate = require('mongoose-validator');
+
+const linkValidator = [
+  validate({
+    validator: 'isURL',
+    message: 'There should be a link in this field',
+  }),
+];
+
+
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: linkValidator,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: linkValidator,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+    select: false,
+  },
+
+});
+
+module.exports = mongoose.model('article', articleSchema);
