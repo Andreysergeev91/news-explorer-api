e114cd58f5aaf1650f68c636ad25d53127aa4d51
Andrey Sergeev <andreyser07@gmail.com>
2020-08-16 16:44:22 +0700

    Change sameSite option in login

--- controllers/users.js
+++ controllers/users.js
@@ -34,7 +34,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 168 * 3600000,
         httpOnly: true,
-        sameSite: 'None',
+        sameSite: true,
         secure: true,
       });
       res.send({ token });
