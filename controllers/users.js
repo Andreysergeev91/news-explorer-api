9f83b310e26279b0e3710e190a1367a6a9e1eca4
Andrey Sergeev <andreyser07@gmail.com>
2020-08-20 21:32:31 +0700

    Change res.cookies options

--- controllers/users.js
+++ controllers/users.js
@@ -34,8 +34,6 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 168 * 3600000,
         httpOnly: true,
-        secure: true,
-        sameSite: 'none',
       });
       res.send({ token });
     })
